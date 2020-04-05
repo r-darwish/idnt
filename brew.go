1e2c3bee5ef8edcb02c70bd9fe574a0d1b6e2307
Roey Darwish Dror <roey.ghost@gmail.com>
2020-04-05 11:08:28 +0300

    Brew

---
+++ brew.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/thoas/go-funk"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+)
+
+type Brew struct {
+}
+
+func (s Brew) Exists() bool {
+	_, err := exec.LookPath("brew")
+	return runtime.GOOS == "darwin" && err == nil
+}
+
+func (s Brew) Name() string {
+	return "Homebrew"
+}
+
+func (s Brew) Packages() ([]string, error) {
+	out, err := exec.Command("brew", "leaves").Output()
+	if err != nil {
+		return nil, err
+	}
+
+	var result = funk.Filter(strings.Split(string(out), "\n"), func(e string) bool {
+		return e != ""
+	}).([]string)
+	return result, nil
+}
+
+func (s Brew) Uninstall(pkg string) error {
+	cmd := exec.Command("brew", "rmtree", pkg)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
