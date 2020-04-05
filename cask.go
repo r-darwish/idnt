a9c9d1f78adb9c25e697137a9afeae0242756c1e
Roey Darwish Dror <roey.ghost@gmail.com>
2020-04-05 11:11:12 +0300

    Brew cask

---
+++ cask.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/thoas/go-funk"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+)
+
+type Cask struct {
+}
+
+func (s Cask) Exists() bool {
+	_, err := exec.LookPath("brew")
+	return runtime.GOOS == "darwin" && err == nil
+}
+
+func (s Cask) Name() string {
+	return "Cask"
+}
+
+func (s Cask) Packages() ([]string, error) {
+	out, err := exec.Command("brew", "cask", "list").Output()
+	if err != nil {
+		return nil, err
+	}
+
+	var result = funk.Filter(strings.Split(string(out), "\n"), func(e string) bool {
+		return e != ""
+	}).([]string)
+	return result, nil
+}
+
+func (s Cask) Uninstall(pkg string) error {
+	cmd := exec.Command("brew", "cask", "uninstall", pkg)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
--- pacakge_managers.go
+++ pacakge_managers.go
@@ -12,7 +12,7 @@ type PackageManager interface {
 var PackageManagers = GetPackageManager()
 
 func GetPackageManager() []PackageManager {
-	return funk.Filter([]PackageManager{Scoop{}, Chocolatey{}, Pacman{}, Brew{}}, func(p PackageManager) bool {
+	return funk.Filter([]PackageManager{Scoop{}, Chocolatey{}, Pacman{}, Brew{}, Cask{}}, func(p PackageManager) bool {
 		return p.Exists()
 	}).([]PackageManager)
 }
