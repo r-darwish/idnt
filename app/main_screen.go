ea65d737b87aa8734634cb6d28d439301be71bdc
Roey Darwish Dror <roey.ghost@gmail.com>
2022-02-11 18:30:22 +0200

    Implement marking

--- app/main_screen.go
+++ app/main_screen.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -13,23 +14,49 @@ var (
 	appStyle = lipgloss.NewStyle().Padding(1, 2)
 )
 
-type AppItem providers.Application
+type listKeyMap struct {
+	execute    key.Binding
+	selectItem key.Binding
+}
+
+func newListKeyMap() *listKeyMap {
+	return &listKeyMap{
+		selectItem: key.NewBinding(
+			key.WithKeys("tab", " ", "x"),
+			key.WithHelp("tab", "select item"),
+		),
+		execute: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter", "uninstall selected apps"),
+		),
+	}
+}
+
+type AppItem struct {
+	app    providers.Application
+	marked bool
+}
 
 func (a AppItem) FilterValue() string {
-	return a.Name
+	return a.app.Name
 }
 
 func (a AppItem) Title() string {
-	return a.Name
+	title := a.app.Name
+	if a.marked {
+		title = "> " + title
+	}
+	return title
 }
 
 func (a AppItem) Description() string {
-	return a.ExtraInfo["Description"]
+	return a.app.ExtraInfo["Description"]
 }
 
 type mainScreenModel struct {
 	list list.Model
 	done bool
+	keys *listKeyMap
 }
 
 func (m mainScreenModel) Init() tea.Cmd {
@@ -46,12 +73,12 @@ func (m mainScreenModel) Init() tea.Cmd {
 				continue
 			}
 			for _, app := range providerApps {
-				allApps = append(allApps, AppItem(app))
+				allApps = append(allApps, &AppItem{app, false})
 			}
 		}
 
 		sort.Slice(allApps, func(i, j int) bool {
-			return strings.ToLower(allApps[i].(AppItem).Name) > strings.ToLower(allApps[j].(AppItem).Name)
+			return strings.ToLower(allApps[i].(*AppItem).app.Name) > strings.ToLower(allApps[j].(*AppItem).app.Name)
 		})
 
 		return allApps
@@ -68,6 +95,22 @@ func (m mainScreenModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.done = true
 		cmd := m.list.SetItems(msg)
 		return m, cmd
+
+	case tea.KeyMsg:
+		if m.list.FilterState() == list.Filtering {
+			break
+		}
+
+		switch {
+		case key.Matches(msg, m.keys.selectItem):
+			item := m.list.SelectedItem().(*AppItem)
+			item.marked = !item.marked
+
+			index := m.list.Index()
+			if index < len(m.list.Items())-1 {
+				m.list.Select(index + 1)
+			}
+		}
 	}
 
 	newListModel, cmd := m.list.Update(msg)
@@ -84,10 +127,19 @@ func (m mainScreenModel) View() string {
 }
 
 func newMainScreen() tea.Model {
+	listKeys := newListKeyMap()
 	l := list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0)
 	l.Title = "Select applications to uninstall"
+	l.AdditionalFullHelpKeys = func() []key.Binding {
+		return []key.Binding{
+			listKeys.execute,
+			listKeys.selectItem,
+		}
+	}
+
 	return mainScreenModel{
 		list: l,
 		done: false,
+		keys: listKeys,
 	}
 }
